246de9d6378b981419c74ccfd95df84ab5f9b245
leocodeio <basicleo787@gmail.com>
2025-01-04 12:15:09 +0000

    first commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { HealthModule } from './health/health.module';
+import { PrometheusService } from './health/prometheus.service';
+import { AppModule } from './app.module';
+import { AppService } from './app.service';
+
+export { HealthModule, PrometheusService, AppModule, AppService };
